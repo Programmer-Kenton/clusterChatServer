3794147f2c335bfaf5f3c9704608a429d7bdeea0
Kenton <1441630907@qq.com>
2024-09-12 10:10:37 +0800

    第十七次提交:客户端开发加好友和聊天功能及测试

---
+++ build/src/client/CMakeFiles/ChatClient.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/usr/local/WorkSpace/clusterChatServer/bin/ChatClient"
+  "/usr/local/WorkSpace/clusterChatServer/bin/ChatClient.pdb"
+  "CMakeFiles/ChatClient.dir/main.cpp.o"
+  "CMakeFiles/ChatClient.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChatClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
