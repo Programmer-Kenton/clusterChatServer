4b36b9764764e9f851876453094debb8a94a0f33
Kenton <1441630907@qq.com>
2024-09-12 19:50:59 +0800

    第二十二次提交:测试脚本构建Cmake并编译

---
+++ build/src/server/CMakeFiles/ChatServer.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "/usr/local/WorkSpace/clusterChatServer/bin/ChatServer"
+  "/usr/local/WorkSpace/clusterChatServer/bin/ChatServer.pdb"
+  "CMakeFiles/ChatServer.dir/ChatServer.cpp.o"
+  "CMakeFiles/ChatServer.dir/ChatServer.cpp.o.d"
+  "CMakeFiles/ChatServer.dir/ChatService.cpp.o"
+  "CMakeFiles/ChatServer.dir/ChatService.cpp.o.d"
+  "CMakeFiles/ChatServer.dir/__/db/DataBase.cpp.o"
+  "CMakeFiles/ChatServer.dir/__/db/DataBase.cpp.o.d"
+  "CMakeFiles/ChatServer.dir/__/model/GroupModel.cpp.o"
+  "CMakeFiles/ChatServer.dir/__/model/GroupModel.cpp.o.d"
+  "CMakeFiles/ChatServer.dir/__/model/UserModel.cpp.o"
+  "CMakeFiles/ChatServer.dir/__/model/UserModel.cpp.o.d"
+  "CMakeFiles/ChatServer.dir/__/model/friendModel.cpp.o"
+  "CMakeFiles/ChatServer.dir/__/model/friendModel.cpp.o.d"
+  "CMakeFiles/ChatServer.dir/__/model/offLineMessgeModel.cpp.o"
+  "CMakeFiles/ChatServer.dir/__/model/offLineMessgeModel.cpp.o.d"
+  "CMakeFiles/ChatServer.dir/__/redis/Redis.cpp.o"
+  "CMakeFiles/ChatServer.dir/__/redis/Redis.cpp.o.d"
+  "CMakeFiles/ChatServer.dir/main.cpp.o"
+  "CMakeFiles/ChatServer.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChatServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
